401df993c4510b4347c4081d1d5ac42cb04ac959
Elliot Cunningham <elliot.cunningham@jdc.fr>
2018-11-10 22:25:43 +0100

    Initial Commit

---
+++ party-song/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  party-song
+//
+//  Created by Elliot Cunningham on 10/11/2018.
+//  Copyright © 2018 Elliot Cunningham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
